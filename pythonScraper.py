80d37d603e999d317f0dcb08285edfea4e6bb8f6
izzimoo <moultoni@tcd.ie>
2022-11-03 14:48:20 +0000

    pythonScraper.py
    First attempt of scraping Python, some issues with description but otherwise works.

---
+++ pythonScraper.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import re
+import csv
+
+url = "https://www.daft.ie/property-for-rent/dublin?showMap=false&sort=priceAsc"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+usableLinks = []
+liData=soup.find_all('a')
+for link in liData:
+    if link.has_attr('href'):
+        usableLinks.append(link['href'])
+
+usableLinks = usableLinks[45:63]
+
+#44:63  usable links from the main page
+
+addressResultString = []
+bedResultString = []
+bathResultString = []
+propertyTypeResultString = []
+priceResultString = []
+descriptionResultsString = []
+
+f = csv.writer(open('testing.csv ','w'))
+for link in usableLinks:
+    url2 = 'https://www.daft.ie/' + link
+    response = requests.get(url2)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    description = soup.find_all('div', attrs={'data-testid':'description', 'class': 'styles__StandardParagraph-sc-15fxapi-8 eMCuSm'})
+    price = soup.find_all('span', attrs={'class':'TitleBlock__StyledSpan-sc-1avkvav-5 fKAzIL'})
+    address = soup.find_all('h1', attrs={'data-testid':'address'})
+    bedroom = soup.find_all('p', attrs={'data-testid':'beds'})
+    bathroom = soup.find_all('p', attrs={'data-testid':'baths'})
+    property_type = soup.find_all('p', attrs={'data-testid':'property-type'})
+
+    descriptionResults = re.search('>(.*)<', str(description))
+    if descriptionResults != None:
+        descriptionResultsString.append(str(descriptionResults.group(1)))
+    else:
+        descriptionResultsString.append('')
+
+    addressResult = re.search('>(.*)<', str(address))
+    if addressResult != None:
+        addressResultString.append(str(addressResult.group(1)))
+    else:
+        addressResultString.append('')
+
+    bedResult = re.search('>(.*)Bed<', str(bedroom))
+    if bedResult != None:
+        bedResultString.append(str(bedResult.group(1)))
+    else:
+        bedResultString.append('0')
+
+    bathResult = re.search('>(.*)Bath<', str(bathroom))
+    if bathResult != None:
+        bathResultString.append(str(bathResult.group(1)))
+    else:
+        bathResultString.append('0')
+
+    propertyTypeResult = re.search('>(.*)<', str(property_type))
+    if propertyTypeResult != None:
+        propertyTypeResultString.append(str(propertyTypeResult.group(1)))
+    else:
+        propertyTypeResultString.append('')
+
+    priceResult = re.search('>(.*)<!-- -->', str(price))
+    if priceResult != None:
+        priceResultString.append(str(priceResult.group(1)))
+    else:
+        priceResultString.append('')
+
+columnsValues  = ['Address', 'Bedroom', 'Bathroom', 'PropertyType', 'Price']
+
+dict = {'Address': addressResultString, 'Bedroom': bedResultString,'Bathroom': bathResultString, 
+        'PropertyType': propertyTypeResultString, 'Price': priceResultString, 'Description': descriptionResultsString}
+
+df = pd.DataFrame(dict)
+
+df.to_csv('testing.csv')
+
+        
