218d0498f9147952026f4a300eb3c0d3ebd1a732
igoea20 <64656258+igoea20@users.noreply.github.com>
2022-11-28 17:35:06 +0000

    Updated variables using cross-validation

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,16 +4,32 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 import string
 import re
 from collections import Counter
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 from sklearn import svm
-
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score, f1_score
 poppedindexes = []
 test_data = []
+
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 
 # takes in the Dublin postcode and returns the score
 def address_labels(address):
@@ -76,29 +92,102 @@ def text_process(text):
     return cleaned
 
 
-## should take in a list of strings containing all the vectors. These need to be cleaned first. Should probably include
-## stemmin etc
-def tf_idf(processed_text):
+#this function was used to perform cross validation on the sentiment analysis model
+def cross_val():
+    processed_training_data = []
+    labels = []
+    #get data
+    print('Reading from pre-processed file: ', 'ProcessedText.csv')
+    with open('ProcessedText.csv','r') as csvfile:
+        c = csv.reader(csvfile, delimiter = ',')
+        next(csvfile)
+        for row in c:
+            processed_training_data.append(row[1])
+            labels.append(int(row[2]))
 
-    vectoriser = TfidfVectorizer()
-    X = vectoriser.fit_transform(processed_text)
+    df_values = [1,2,3,4,5]
+    c = 8
+    predictions = []
+    mean_f1=[]
+    std_acc = []
 
-    return vectoriser
+    #first test the min_df
+    for df in df_values:
+        model_svm, tfidf_vec, X_train_bow = train_dataset(processed_training_data, labels, df, c)
+        temp = []
+        kf = KFold(n_splits=10)
 
+        processed_training_data = np.array(processed_training_data)
+        labels = np.array(labels)
+        for train, test in kf.split(processed_training_data):
+            X_train_bow = tfidf_vec.transform(processed_training_data[train]) # fit train
+            model_svm.fit(X_train_bow, labels[train])
+            X_test_bow = tfidf_vec.transform(processed_training_data[test]) # fit train
+            ypred = model_svm.predict(X_test_bow)
+            temp.append(f1_score(labels[test], ypred, average='weighted'))
+        mean_f1.append(np.array(temp).mean())
+        std_acc.append(np.array(temp).std())
+
+
+    plt.errorbar(df_values, mean_f1, yerr=std_acc, marker = 's', ms = 5, ecolor='r', color = 'k')
+    plt.legend()
+    plt.xlabel(r'Cut off ($min\_df$)')
+    plt.xticks(df_values)
+    plt.ylabel('F1 Score')
+    plt.title('Cross-validation for the TfIDF vectorizer')
+    plt.show()
+
+
+    #then setting df to be ==2 based on the cross validation
+    c_values = [0.5, 2, 4, 6, 8, 10]
+    predictions = []
+    mean_f1=[]
+    std_acc = []
+
+    df = 2
+
+
+    #first test the min_df
+    for c in c_values:
+        model_svm, tfidf_vec, X_train_bow = train_dataset(processed_training_data, labels, df, c)
+        temp = []
+        kf = KFold(n_splits=10)
+
+        processed_training_data = np.array(processed_training_data)
+        labels = np.array(labels)
+        for train, test in kf.split(processed_training_data):
+            X_train_bow = tfidf_vec.transform(processed_training_data[train]) # fit train
+            model_svm.fit(X_train_bow, labels[train])
+            X_test_bow = tfidf_vec.transform(processed_training_data[test]) # fit train
+            ypred = model_svm.predict(X_test_bow)
+            temp.append(f1_score(labels[test], ypred, average='weighted'))
+        mean_f1.append(np.array(temp).mean())
+        std_acc.append(np.array(temp).std())
+
+            #    model_svm.fit(X_train_bow, y_train)
+
+        #we will look at the overall accuracy
+    plt.errorbar(c_values, mean_f1, yerr=std_acc, marker = 's', ms = 5, ecolor='r', color = 'k')
+    plt.legend()
+    plt.xlabel(r'Penalty Weighting, $C$')
+    plt.xticks(c_values)
+    plt.ylabel('F1 Score')
+    plt.title('Cross-validation for the SVM')
+    plt.show()
+    return 0
 
 ## takes in the already processed training data and trains the svm. Returns the trained svm and tfidf.
-def train_dataset(processed_training_data, labels):
+def train_dataset(processed_training_data, labels, df, c):
 
     X_train = processed_training_data
     y_train = labels
 
-    tfidf_vec = TfidfVectorizer(min_df = 1, token_pattern = r'[a-zA-Z]+')
+    tfidf_vec = TfidfVectorizer(min_df = df, token_pattern = r'[a-zA-Z]+')  #1
     X_train_bow = tfidf_vec.fit_transform(X_train) # fit train
-    model_svm = svm.SVC(C=8.0, kernel='linear')
+    model_svm = svm.SVC(C=c, kernel='linear')  #8
     model_svm.fit(X_train_bow, y_train)
-    #predictions = model_svm.predict(X_train_bow)
 
-    return model_svm, tfidf_vec
+    return model_svm, tfidf_vec, X_train_bow
 
 
 #takes in the processed text and a pretrained svm and tfidf and predicts the address labels
@@ -169,7 +258,7 @@ def predict_address(fileName, test_data):
             test_data.remove(test_data[index])
 
         #training
-        model_svm, tfidf = train_dataset(processed_training_data, labels)
+        model_svm, tfidf, _ = train_dataset(processed_training_data, labels, 2, 8)
         processed_test_data = text_process(test_data)
         print('length of processed_test_data: ', len(processed_test_data))
         predicted_labels = generate_svm_scores(processed_test_data, model_svm, tfidf)
@@ -270,5 +359,8 @@ with open('CleanedScraperOutput.csv','r') as csvfile:
         if row[8] != 'Description':
             test_data.append(str(row[8]))
 
+#fig, axs = plt.subplots(1,2, sharey=True)
+
 #Create new CSV file for neural data
 neuralData()
+#cross_val()
